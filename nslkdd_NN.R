5e2762ab798d6a55fa00844d3fe7b1da437bb7b3
johnleinenveber <34614539+johnleinenveber@users.noreply.github.com>
2018-02-27 08:50:19 -0800

    Update nslkdd_NN.R

--- nslkdd_NN.R
+++ nslkdd_NN.R
@@ -1,4 +1,3 @@
-setwd("Documents/MIS545/FinalProject")
 
 #install.packages('ISLR')
 #install.packages('caTools')
@@ -7,25 +6,24 @@ library(ISLR)
 library(caTools)
 library(neuralnet)
 
+start <- Sys.time()
+
 nslkdd_training_set <- read.csv("NSLKDD/KDDtrain_20Percent_withCols.csv")
 
-nslkdd_training_set$protocol_type <- as.numeric(nslkdd_training_set$protocol_type)
-nslkdd_training_set$service <- as.numeric(nslkdd_training_set$service)
-nslkdd_training_set$flag <- as.numeric(nslkdd_training_set$flag)
-nslkdd_training_set$attack_type <- as.numeric(nslkdd_training_set$attack_type)
+nslkdd_training_set$protocol_type <- as.integer(as.numeric(nslkdd_training_set$protocol_type))
+nslkdd_training_set$service <- as.integer(as.numeric(nslkdd_training_set$service))
+nslkdd_training_set$attack_type <- ifelse(nslkdd_training_set$attack_type == "normal", 0, 1)
 
-nslkdd_training_set$src_bytes <- as.numeric(nslkdd_training_set$src_bytes)
-nslkdd_training_set$dst_bytes <- as.numeric(nslkdd_training_set$dst_bytes)
-nslkdd_training_set$count <- as.numeric(nslkdd_training_set$count)
-nslkdd_training_set$srv_count <- as.numeric(nslkdd_training_set$srv_count)
-nslkdd_training_set$dst_host_count <- as.numeric(nslkdd_training_set$dst_host_count)
-nslkdd_training_set$dst_host_srv_count <- as.numeric(nslkdd_training_set$dst_host_srv_count)
+maxs <- apply(nslkdd_training_set[,1:8], 2, max)
+mins <- apply(nslkdd_training_set[,1:8], 2, min)
+scaled_data <- as.data.frame(scale(nslkdd_training_set[,1:8], center = mins, scale = maxs - mins))
 
-maxs <- apply(nslkdd_training_set[,1:24], 2, max)
-mins <- apply(nslkdd_training_set[,1:24], 2, min)
+Attack <- as.integer(nslkdd_training_set$attack_type)
+data <- cbind(Attack, scaled_data)
 
-scaled_data <- as.data.frame(scale(nslkdd_training_set[,1:24], center = mins, scale = maxs - mins))
-data <- cbind(scaled_data, nslkdd_training_set$attack_type)
+sample_size <- 10000
+index <- sample(nrow(data), size = sample_size, replace = FALSE)
+data <- data[index,]
 
 sample_size <- floor(0.7 * nrow(data))
 training_index <- sample(nrow(data), size = sample_size, replace = FALSE)
@@ -34,17 +32,18 @@ test <- data[-training_index,]
 
 feats <- names(scaled_data)
 f <- paste(feats, collapse = '+')
-f <- paste('nslkdd_training_set$attack_type ~', f)
+f <- paste('Attack ~', f)
 f <- as.formula(f)
 
-nn <- neuralnet(f, train, linear.output = FALSE)
+nn <- neuralnet(f, train, hidden = c(5,5,5), linear.output = FALSE)
 
-predicted.nn.values$net.result <- sapply(predicted.nn.values$net.result, round, digits = 0)
+predicted_nn <- compute(nn,test[2:9])
+predicted_nn$net.result <- sapply(predicted_nn$net.result, round, digits = 0)
+table(test$Attack,predicted_nn$net.result)
+plot(nn)
 
-table(test$Private, predicted.nn.values$net.result)
-
-
-neuralnetwork <- neuralnet(f, train, hidden = c(10,10,10), linear.output = FALSE)
+end <- Sys.time()
+end - start
 
 
 
